5277f1bc99eac57da05961240e9fce2f07e38255
RadGar1 <garrettraddohl@gmail.com>
2024-09-20 03:31:17 -0500

    homework 1

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,18 @@
 
-
+char glbl[128];
+char huge_array[8192];
 
 void kernel_main() {
-//comment
+
+	extern int __bss_start, __bss_end;
+	char *bssstart, *bssend;
+
+	bssstart = &__bss_start;
+	bssend = &__bss_end;
+
+	for (int *i = bssstart; *i < bssend; *i++){
+		*i = 0;
+	}
 
     while(1){
     }
